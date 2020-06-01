404ab8bdd7d5bf0ce1b02ca263e200330a500ba3
Vlad Kononov <vladimirkon@gmail.com>
2020-06-01 14:22:58 -0700

    added java case

---
+++ src/test/ReverseMethod.java
@@ -0,0 +1,22 @@
+package test;
+
+public class ReverseMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name = "civic";
+		String reverse = "";
+		for(int count= name.length()-1; count >= 0; count--) {
+			reverse = reverse + name.charAt(count);
+		}
+		System.out.println(reverse);
+		
+		if(name.equalsIgnoreCase(reverse)) {
+			System.out.println(name + " is a palindrome word");
+		} else {
+			System.out.println(name + " is NOT a palindrome word!!!");
+		}
+		
+	}
+
+}
